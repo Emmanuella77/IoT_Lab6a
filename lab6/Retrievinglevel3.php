4a4cf18474ea0bd710f61b120933a92471bc55ad
Emmanuella77 <emmanuella.nketia@ashesi.edu.gh>
2022-11-30 02:13:32 +0000

    Initial commit

---
+++ lab6/Retrievinglevel3.php
@@ -0,0 +1,26 @@
+<?php
+    $servername = "localhost";
+    $username = "root";
+    $password = "";
+    $dbname = "lab6";
+
+    $con = mysqli_connect($servername,$username,$password,$dbname);
+    
+    $data=array(); 
+
+
+   $sql = "SELECT * FROM (
+    SELECT * 
+    FROM data_sensors 
+    WHERE (ESP_ID = 0)  ORDER BY ID DESC LIMIT 5
+  ) AS `table` ORDER by ID ASC";
+
+//check what ID is 
+
+   if ($q=mysqli_query($con, $sql)) {
+    while ($row = $q->fetch_assoc()){
+        echo "{$row['Distance']}  {$row['Timeread']} \n";
+    }
+   } 
+   
+?>
